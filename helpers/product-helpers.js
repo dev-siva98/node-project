8eb51289522019a41d8809e695ab3c855db022c5
Siva <sivaprasadtppba@gmail.com>
2021-10-01 22:20:52 +0530

    30-09

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -2,9 +2,14 @@ var db=require('../config/connection')
 module.exports={
 
     addProduct:(product,callback)=>{
-        db.get().collection('product').insertOne(product,function(err,data){
+        db.get().collection('product').insertOne(product).then((data)=>{
             callback(data.insertedId)
 
+        })
+    },
+    getAllProducts:()=>{
+        return new Promise((resolve,reject)=>{
+            let products=db.get().collection()
         })
     }
 
